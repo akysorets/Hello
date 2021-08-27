df57397a829067631bb85a1fd85211a82a0916bf
Albina Kysorets <akysorets2@huskers.unl.edu>
2021-08-27 16:30:59 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Albina Kysorets
+ *
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Albina Kysorets\n");
+
+  return 0;
+}
